242f79244274df7854fa80a5010c1077ce789ca4
Niki7 <121088976+NiKi4856853938578467@users.noreply.github.com>
2025-03-05 13:40:14 +0100

    Add files via upload

---
+++ ARTICLE PREVIEW COMPONENT/JS/Tariq.js
@@ -0,0 +1,15 @@
+function boton(){
+
+    document.getElementById("activo").style.display = "none";
+    document.getElementById("inactivo").style.display = "contents";
+
+    
+}
+function boton2(){
+
+    document.getElementById("inactivo").style.display = "none"
+    document.getElementById("activo").style.display = "contents"
+
+
+
+}
